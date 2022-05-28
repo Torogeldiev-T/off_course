5b207f099a48b0b6bde90ad41ffd8fdd4d0d754b
Torogeldiev-T <tilek2402@gmail.com>
2022-05-28 15:54:21 +0600

    user auth finished

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
--- off_course/urls.py
+++ off_course/urls.py
@@ -16,14 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
+from accounts.views import RegisterAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path(
-        "api/token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"
+        "api/login/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"
     ),
     path(
-        "api/token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"
+        "api/login/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"
     ),
+    path("api/register/", RegisterAPIView.as_view(), name="api_register"),
 ]
